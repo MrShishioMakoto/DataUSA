0c4b45ba03f5cb3f86b8d81c68e1d07a56dd831c
Goncalo Almeida <goncalo.almeida88@gmail.com>
2024-07-31 13:35:29 +0100

    Initial Commit

---
+++ DataUSA/DataUSAApp.swift
@@ -0,0 +1,17 @@
+//
+//  DataUSAApp.swift
+//  DataUSA
+//
+//  Created by GoncaloSAlmeida on 31/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct DataUSAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
